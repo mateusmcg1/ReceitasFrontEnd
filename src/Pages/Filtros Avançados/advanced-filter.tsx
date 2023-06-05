e91471e54d74ce52094845062ba608f1ea790173
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2023-06-05 14:50:38 -0300

    inicio do filtro avançado

--- src/Pages/Filtros Avançados/advanced-filter.tsx
+++ src/Pages/Filtros Avançados/advanced-filter.tsx
@@ -1,2 +1,20 @@
 import { Slider } from 'primereact/slider';
 import './advanced-filter.css'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+export default function AdvancedFilter() {
+
+    let navigate = useNavigate()
+
+    return(
+
+        <div className='filter-container'>
+
+            <h1>Filtros Avançados</h1>
+
+        </div>
+    )
+
+}
