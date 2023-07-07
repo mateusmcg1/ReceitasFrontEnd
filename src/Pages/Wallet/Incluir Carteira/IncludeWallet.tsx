e3a5e34deb855e7f5403d8058942c1cd914e7863
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2023-07-07 16:35:13 -0300

    fixing currencies options

--- src/Pages/Wallet/Incluir Carteira/IncludeWallet.tsx
+++ src/Pages/Wallet/Incluir Carteira/IncludeWallet.tsx
@@ -7,6 +7,7 @@ import { Toast, ToastMessage } from 'primereact/toast';
 import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect';
 import { CurrencyDto } from '../../../models/currenty.dto';
 import { CurrencyEnum } from '../../../Shared/enums/CurrencyEnum';
+import { Dropdown } from 'primereact/dropdown';
 
 
 export default function IncludeWallet() {
@@ -15,8 +16,6 @@ export default function IncludeWallet() {
     const [text2, setText2] = useState('');
     const toast = useRef<Toast>(null);
     const [selectedCurrency, setSelectedCurrency] = useState<CurrencyDto[]>([]);
-    //É AQUI ONDE EU NAO SEI O QUE FAZER!
-    //var currencyTypes = Object.values(CurrencyEnum);// 
 
     const show = (severity: ToastMessage["severity"], summary: string, detail: string) => {
         toast.current?.show({ severity, summary, detail });
@@ -54,19 +53,21 @@ export default function IncludeWallet() {
             <Toast ref={toast} />
             <h1>Incluir Carteira</h1>
 
-            <div className='inclusao-frame'>
-                <label htmlFor="text1" style={{ marginBottom: "1%" }}>Título</label>
-                <InputText value={text1} onChange={(e) => setText1(e.target.value)} />
 
-                {/* <label htmlFor="text2">Moeda</label> */}
-                <MultiSelect value={selectedCurrency} onChange={(e: MultiSelectChangeEvent) => setSelectedCurrency(e.value)} options={currencyTypes} display="chip" optionLabel="currency"
-                    placeholder="Select Currency" maxSelectedLabels={1} className="w-full md:w-20rem" />
-                {/* <InputText value={text2} onChange={(e) => setText2(e.target.value)} /> */}
-
-
-                <div className='inclusao-button'>
-                    {<Button label="INCLUIR" onClick={addWallets} />}
+            <div className="grid">
+                <div className="col">
+                    <label htmlFor="text1" style={{ marginBottom: "1%" }}>Nome</label>
+                    <InputText value={text1} onChange={(e) => setText1(e.target.value)} />
                 </div>
+            </div>
+            <div className="grid">
+                <div className="col">
+                    <Dropdown value={selectedCurrency} onChange={(e) => setSelectedCurrency(e.value)} options={Object.values(CurrencyEnum)}
+                        placeholder="Selecione uma Moeda" />
+                </div>
+            </div>
+            <div className="flex justify-content-end">
+                <Button label="INCLUIR" onClick={addWallets} />
             </div>
 
         </div>
